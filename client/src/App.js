95dc8c5ba764bd88f9e2ce90cf18c9ead14bf1ab
Paul McGlinchey <paul.mcglinchey@eso.com>
2021-11-15 22:21:38 +0000

    Fixed errors

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,14 @@ import RestaurantList from './components/RestaurantList';
 
 function App() {
 
-  const [cuisine, setCuisine] = useState('');
+  const [cuisine, setCuisine] = useState('Cuisine');
 
   return (
     <div className="App">
       <div className="container mx-auto mt-5 border-4 rounded p-2 border-black">
         <div className="flex justify-between">
           <h1 className="text-3xl font-mono font-bold">ClientSplash</h1>
-          <DropdownMenu setCuisine={() => setCuisine()}>Cuisine</DropdownMenu>
+          <DropdownMenu setCuisine={() => setCuisine()}>{cuisine}</DropdownMenu>
         </div>
         <RestaurantList />
       </div>
