e68e9ce025b2ea7addad8854740fe25786189046
Paul McGlinchey <paul.mcglinchey@eso.com>
2021-11-17 12:25:42 +0000

    updated env variable

--- client/src/config/endpoints.js
+++ client/src/config/endpoints.js
@@ -1,4 +1,4 @@
-const BASE_URL = (process.env.CLIENTSPLASH_API || 'http://localhost:3001') + '/api/'
+const BASE_URL = process.env.CLIENTSPLASH_API || 'http://localhost:3001/api/';
 
 const ENDPOINTS = {
     "restaurants": BASE_URL + "restaurants"
