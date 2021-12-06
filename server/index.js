ddf038a6536d14c47b5129de5f8b9ec3b403df36
Paul McGlinchey <paul.mcglinchey@eso.com>
2021-12-06 12:53:41 +0000

    updated origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/', (req, res, next) => {
-  const origin = (req.headers.origin == 'http://localhost:3000') ? 'http://localhost:3000' : 'https://mywebsite.com'
+  const origin = (req.headers.origin == 'http://localhost:3000') ? 'http://localhost:3000' : 'https://clientsplash.herokuapp.com/'
   res.setHeader('Access-Control-Allow-Origin', origin)
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
