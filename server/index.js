f80998404428727e62e303973a8627509dd1afe6
Paul McGlinchey <paul.mcglinchey@eso.com>
2021-12-06 12:52:59 +0000

    Added headers to all responses

--- server/index.js
+++ server/index.js
@@ -9,6 +9,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use('/', (req, res, next) => {
+  const origin = (req.headers.origin == 'http://localhost:3000') ? 'http://localhost:3000' : 'https://mywebsite.com'
+  res.setHeader('Access-Control-Allow-Origin', origin)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('Welcome to the clientsplash API')
 })
